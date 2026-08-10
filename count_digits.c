6a8f4f93ab3d751d8b6e6fb4ebdb1f56695881eb
SUYASH-27-UI <suyashsurve30@gmail.com>
2026-08-10 20:48:02 +0530

    Create count_digits.c

---
+++ count_digits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}
